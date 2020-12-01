4dd0212929e84e0ced4429637dac5c34c9afc783
Hector M Gerardo <hectormgerardo@gmail.com>
2020-11-30 21:29:33 -0700

    ScenathonWeb added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ScenathonWeb from './pages/ScenathonWeb';
 
 export { default as ACTIONS } from './constants/actions';
 export { default as EVENTS } from './constants/events';
@@ -11,10 +13,13 @@ export { default as LIFECYCLE } from './constants/lifecycle';
 export { default as STATUS } from './constants/status';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Router>
+		<Route path="/" component={App} >
+			<Route path="/ScenathonWeb" component={ScenathonWeb}>
+			</Route>
+		</Route>
+	</Router>,
+	document.getElementById('root')
 );
 
 
