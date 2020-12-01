d957941ebcfa07586d59f54b6d53eeec5c58173b
Hector M Gerardo <hectormgerardo@gmail.com>
2020-11-30 21:36:28 -0700

    improt stlyes

---
+++ src/pages/ScenathonWeb.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/index.css';
+
+class ScenathonWeb extends Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return(
+			<div className="container">
+				<p>LOL</p>
+			</div>
+		);
+	}
+} 
+
+export default ScenathonWeb;
